942615224eb255cc20dbad6558685903df6be456
Gabo <gabrielathie@hotmail.com>
2025-05-16 11:26:38 -0600

    Uses NavLink for routing in NavBar
    Updates NavBar component to use NavLink for routing,
    ensuring proper navigation within the application.

--- sqe-games/src/components/NavBar.js
+++ sqe-games/src/components/NavBar.js
@@ -8,18 +8,18 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/SQEGames">SQE Games</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">SQE Games</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/SQEGames/puzzle1">Puzzle 1</Nav.Link>
-            <Nav.Link href="/SQEGames/puzzle2">Puzzle 2</Nav.Link>
-            <Nav.Link href="/SQEGames/puzzle3">Puzzle 3</Nav.Link>
-            <Nav.Link href="/SQEGames/puzzle4">Puzzle 4</Nav.Link>
-            <Nav.Link href="/SQEGames/puzzle5">Puzzle 5</Nav.Link>
-            <Nav.Link href="/SQEGames/puzzle6">Puzzle 6</Nav.Link>
-            <Nav.Link href="/SQEGames/puzzle7">Puzzle 7</Nav.Link>
-            <Nav.Link href="/SQEGames/escape_room">Escape Room</Nav.Link>
+            <Nav.Link as={NavLink} to="/puzzle1">Puzzle 1</Nav.Link>
+            <Nav.Link as={NavLink} to="/puzzle2">Puzzle 2</Nav.Link>
+            <Nav.Link as={NavLink} to="/puzzle3">Puzzle 3</Nav.Link>
+            <Nav.Link as={NavLink} to="/puzzle4">Puzzle 4</Nav.Link>
+            <Nav.Link as={NavLink} to="/puzzle5">Puzzle 5</Nav.Link>
+            <Nav.Link as={NavLink} to="/puzzle6">Puzzle 6</Nav.Link>
+            <Nav.Link as={NavLink} to="/puzzle7">Puzzle 7</Nav.Link>
+            <Nav.Link as={NavLink} to="/escape_room">Escape Room</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
